39300c020d50cdaf246135dacb6db48eddaaa423
Steve Gossip <stephengossip@gmail.com>
2019-08-11 18:24:13 +0100

    refactored instrument_families model to use es6 class structure.

--- client/src/models/instrument_families.js
+++ client/src/models/instrument_families.js
@@ -1,21 +1,43 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const InstrumentFamilies = function (data) {
-  this.data = data;
-};
+class InstrumentFamilies {
+  constructor(data) {
+    this.data = data;
+  }
 
-InstrumentFamilies.prototype.bindEvents = function () {
-  PubSub.publish('InstrumentFamilies:data-ready', this.data);
+  bindEvents() {
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
 
-  PubSub.subscribe('SelectView:change', (evt) => {
-    const selectedIndex = evt.detail;
-    this.publishFamilyDetail(selectedIndex);
-  });
-};
+    PubSub.subscribe('SelectView:change', (evt) => {
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  }
 
-InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
-  const selectedFamily = this.data[selectedIndex];
-  PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
-};
+  publishFamilyDetail(selectedIndex) {
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  }
+
+
+
+}
+// const InstrumentFamilies = function (data) {
+//   this.data = data;
+// };
+//
+// InstrumentFamilies.prototype.bindEvents = function () {
+//   PubSub.publish('InstrumentFamilies:data-ready', this.data);
+//
+//   PubSub.subscribe('SelectView:change', (evt) => {
+//     const selectedIndex = evt.detail;
+//     this.publishFamilyDetail(selectedIndex);
+//   });
+// };
+
+// InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+//   const selectedFamily = this.data[selectedIndex];
+//   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+// };
 
 module.exports = InstrumentFamilies;
